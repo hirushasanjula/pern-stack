01797ab92f48d9435b9b96d2ab45cc2dcd592c82
hirushasanjula <10898650@students.plymouth.ac.uk>
2025-06-04 23:39:13 +0530

    navbar update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link, useResolvedPath } from 'react-router-dom'
 import {ShoppingBagIcon, ShoppingCartIcon} from 'lucide-react'
 import ThemeSelector from './ThemeSelector'
+import { useProductStore } from '../stores/productStore'
 
 function Navbar() {
   const {pathname} = useResolvedPath()
   const isHomePage = pathname === '/';
 
-  
+  const {products} = useProductStore();
 
   return (
     <div className='bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50'>
@@ -33,7 +34,7 @@ function Navbar() {
                 <div className='p-2 rounded-full hover:bg-base-200 transition-colors'>
                   <ShoppingBagIcon className='size-5 ' />
                   <span className='badge badge-sm badge-primary indicator-item'>
-                    9
+                    {products.length}
                   </span>
                 </div>
               </div>
